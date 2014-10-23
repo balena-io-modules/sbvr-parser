d1870db3a71c7a9d282fd9116337c6cc12c439de
Pagan Gazzard <pjgazzard@googlemail.com>
2014-10-23 19:29:53 +0100

    UMD for the highlighting helper.

--- highlighting/sbvr.js
+++ highlighting/sbvr.js
@@ -1,3 +1,17 @@
-define(['codemirror-ometa/highlighter', '../sbvr-parser', 'css!./sbvr'], function(codeMirrorOmetaHighlighter, SBVRParser) {
+(function (root, factory) {
+	if (typeof define === 'function' && define.amd) {
+		// AMD. Register as an anonymous module.
+		define(['ometa-js/lib/codemirror-ometa/highlighter', '../sbvr-parser', 'css!./sbvr'], factory);
+	} else if (typeof exports === 'object') {
+		// CommonJS
+		factory(
+			require('ometa-js/lib/codemirror-ometa/highlighter'),
+			require('../sbvr-parser')
+		);
+	} else {
+		// Browser globals
+		factory(root.codeMirrorOmetaHighlighter, root.SBVRParser);
+	}
+}(this, function (codeMirrorOmetaHighlighter, SBVRParser) {
 	codeMirrorOmetaHighlighter(SBVRParser, 'sbvr', 'text/sbvr', {enableLineByLineParsing: true});
 });
